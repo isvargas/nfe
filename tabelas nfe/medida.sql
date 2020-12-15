b1df9c88c3cae493ec5b13ce608e160959e0a0a9
Ivan S. Vargas <contato@is5.com.br>
2020-12-15 07:32:43 -0300

    Add files via upload

---
+++ tabelas nfe/medida.sql
@@ -0,0 +1,37 @@
+﻿CREATE TABLE nfe_medida(
+	id integer primary key auto_increment,
+	codigo varchar(5),
+	descricao varchar(25)
+);
+
+INSERT INTO nfe_medida (codigo,descricao) VALUES 
+('BD','BALDE')
+,('BIS','BISNAGA')
+,('CM','CENTIMETRO')
+,('CT','CENTO')
+,('CX','CAIXA')
+,('DZ','DUZIA')
+,('FD','FARDO')
+,('FR','FRASCO')
+,('GL','GALAO')
+,('GR','GRAMA')
+,('KG','QUILO')
+,('KIT','CONJUNTO')
+,('KU','UNIDADE PESADA')
+,('LT','LITRO')
+,('M2','METRO QUADRADO')
+,('M3','METRO CUBICO')
+,('MG','MILIGRAMAS')
+,('MIL','MILHEIRO')
+,('MM','MILIMETROS')
+,('PAR','PARES')
+,('PC','PECA')
+,('PCT','PACOTE')
+,('PT','POTE')
+,('RL','ROLO')
+,('SC','SACO')
+,('TB','TUBO')
+,('TN','TONELADA')
+,('UN','UNIDADE')
+,('UND','UNIDADES')
+;
---
+++ tabelas nfe/origem.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE nfe_origem(
+	id integer primary key auto_increment,
+	codigo varchar(5),
+	descricao text
+);
+
+INSERT INTO nfe_origem (codigo,descricao) VALUES 
+('0','Nacional, exceto as indicadas nos códigos 3 a 5')
+,('1','Estrangeira - Importação direta, exceto a indicada no código 6')
+,('2','Estrangeira - Adquirida no mercado interno, exceto a indicada no código 7')
+,('3','Nacional, mercadoria ou bem com Conteúdo de Importação superior a 40% (quarenta por cento)')
+,('4','Nacional, cuja produção tenha sido feita em conformidade com os processos produtivos básicos de que tratam o Decreto-Lei nº 288/67, e as Leis nºs 8.248/91, 8.387/91, 10.176/01 e 11 . 4 8 4 / 0 7')
+,('5','Nacional, mercadoria ou bem com Conteúdo de Importação inferior ou igual a 40% (quarenta por cento)')
+,('6','Estrangeira - Importação direta, sem similar nacional, constante em lista de Resolução CAMEX')
+,('7','Estrangeira - Adquirida no mercado interno, sem similar nacional, constante em lista de Resolução CAMEX')
+;
